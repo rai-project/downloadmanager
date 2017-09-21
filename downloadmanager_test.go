fbcecd05ad01349759a636e4f169bc464acc0373
Cheng Li <cli99@illinois.edu>
2017-09-21 12:03:54 -0500

    add test

--- downloadmanager_test.go
+++ downloadmanager_test.go
@@ -6,14 +6,16 @@ import (
 
 	"github.com/rai-project/config"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
 )
 
 func TestDownloadJSON(t *testing.T) {
 	const url = "http://data.dmlc.ml/models/imagenet/inception-bn/Inception-BN-symbol.json"
-	target, err := DownloadFile(url, "/tmp/inception.json")
+	ctx := context.Background()
+	opts := NewOptions(MD5Sum("93ea4544c19709161fec0051aea34885"))
+	target, err := DownloadFile(ctx, url, opts.md5Sum)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, target)
-	assert.Equal(t, "/tmp/inception.json", target)
 }
 
 func TestMain(m *testing.M) {
