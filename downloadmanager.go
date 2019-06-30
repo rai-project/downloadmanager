ba1da121542db77fc12e929de694d3defb5b44f7
Cheng Li <cli99@illinois.edu>
2019-06-29 19:27:08 -0700

    clean up trace

--- downloadmanager.go
+++ downloadmanager.go
@@ -30,11 +30,11 @@ func (c cacheKey) String() string {
 	return fmt.Sprintf("key:%v,path:%v", c.url, c.targetFilePath)
 }
 
-func DownloadFile(url, targetFilePath string, opts ...Option) (string, error) {
+func DownloadFile(url, targetFilePath string, opts ...Option) (string, bool, error) {
 	options := NewOptions(opts...)
 
 	if url == "" {
-		return "", errors.New("invalid empty url")
+		return "", false, errors.New("invalid empty url")
 	}
 
 	cacheKey := cacheKey{url: url, targetFilePath: targetFilePath}.String()
@@ -44,21 +44,21 @@ func DownloadFile(url, targetFilePath string, opts ...Option) (string, error) {
 		if val, found := cache.Get(cacheKey); found {
 			s, ok := val.(string)
 			if ok && com.IsFile(targetFilePath) {
-				return s, nil
+				return s, false, nil
 			}
 		}
 	}
 
 	_, err := getter.Detect(url, targetFilePath, getter.Detectors)
 	if err != nil {
-		return "", err
+		return "", false, err
 	}
 
 	targetDir := cleanup(filepath.Dir(targetFilePath))
 	if !com.IsDir(targetDir) {
 		err := os.MkdirAll(targetDir, 0700)
 		if err != nil {
-			return "", errors.Wrapf(err, "failed to create %v directory", targetDir)
+			return "", false, errors.Wrapf(err, "failed to create %v directory", targetDir)
 		}
 	}
 
@@ -66,13 +66,13 @@ func DownloadFile(url, targetFilePath string, opts ...Option) (string, error) {
 	if options.cache && com.IsFile(targetFilePath) {
 		if options.checkMd5Sum == false {
 			cache.Set(cacheKey, targetFilePath, gocache.DefaultExpiration)
-			return targetFilePath, nil
+			return targetFilePath, false, nil
 		}
 		if options.md5Sum != "" {
 			if ok, err := utils.MD5Sum.CheckFile(targetFilePath, options.md5Sum); err == nil && ok {
 				// Set the value of the key url to targetDir, with the default expiration time
 				cache.Set(cacheKey, targetFilePath, gocache.DefaultExpiration)
-				return targetFilePath, nil
+				return targetFilePath, false, nil
 			}
 		}
 		os.RemoveAll(targetFilePath)
@@ -95,14 +95,14 @@ func DownloadFile(url, targetFilePath string, opts ...Option) (string, error) {
 		Decompressors: map[string]getter.Decompressor{}, // do not decompress
 	}
 	if err := client.Get(); err != nil {
-		return "", err
+		return "", false, err
 	}
 
 	// validate checksum
 	if options.md5Sum != "" && options.checkMd5Sum {
 		if ok, err := utils.MD5Sum.CheckFile(targetFilePath, options.md5Sum); !ok {
 			os.RemoveAll(targetFilePath)
-			return "", err
+			return "", false, err
 		}
 	}
 
@@ -111,7 +111,7 @@ func DownloadFile(url, targetFilePath string, opts ...Option) (string, error) {
 		cache.Set(cacheKey, targetFilePath, gocache.DefaultExpiration)
 	}
 
-	return targetFilePath, nil
+	return targetFilePath, true, nil
 }
 
 func DownloadInto(url, targetDir string, opts ...Option) (string, error) {
@@ -129,23 +129,26 @@ func DownloadInto(url, targetDir string, opts ...Option) (string, error) {
 	if err != nil {
 		return "", errors.Wrapf(err, "unable to parse url %v", url)
 	}
-	t := filepath.Join(targetDir, filepath.Base(urlParsed.Path))
-	filePath, err := DownloadFile(url, t, WithOptions(options))
+	targetFilePath := filepath.Join(targetDir, filepath.Base(urlParsed.Path))
+
+	targetFilePath, ifDownload, err := DownloadFile(url, targetFilePath, WithOptions(options))
 	if err != nil {
-		return "", errors.Wrapf(err, "unable to download url %v into %v", url, t)
+		return "", errors.Wrapf(err, "unable to download url %v into %v", url, targetFilePath)
 	}
 
-	if err := Unarchive(targetDir, filePath); err != nil {
-		return "", err
+	if ifDownload {
+		if err := Unarchive(targetDir, targetFilePath); err != nil {
+			return "", err
+		}
 	}
 
 	if options.cache {
 		// Set the value of the key url to targetDir, with the default expiration time
-		cacheKey := cacheKey{url: url, targetFilePath: filePath}.String()
-		cache.Set(cacheKey, filePath, gocache.DefaultExpiration)
+		cacheKey := cacheKey{url: url, targetFilePath: targetFilePath}.String()
+		cache.Set(cacheKey, targetFilePath, gocache.DefaultExpiration)
 	}
 
-	return filePath, nil
+	return targetFilePath, nil
 }
 
 func Unarchive(targetDir, filePath string) error {
